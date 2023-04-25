776f95ba82bc124959780331b17c941739799bf5
It's me! <bot-m95@mail.ru>
2023-04-25 10:49:21 +0300

    Подключение к БД

--- BusStation/Program.cs
+++ BusStation/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using BusStation.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<CourseDBContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CourseDB")));
 
 var app = builder.Build();
 

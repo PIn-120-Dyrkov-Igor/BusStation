ff745556e522f7745bb02d776f67ae34d4ce2b5f
It's me! <bot-m95@mail.ru>
2023-04-25 10:37:03 +0300

    Добавьте файлы проекта.

---
+++ BusStation/Models/CourseDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BusStation.Models
+{
+    public class CourseDBContext : DbContext
+    {
+        public CourseDBContext(DbContextOptions<CourseDBContext> options) : base(options) {}
+        public DbSet<Passanger> Passers { get; set; }
+    }
+}
---
+++ BusStation/Models/Passanger.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace BusStation.Models
+{
+    public class Passanger
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Фамилия")]
+        [StringLength(20, MinimumLength = 2)]
+        public string Surname { get; set; }
+
+        [Display(Name = "Имя")]
+        [StringLength(20, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Display(Name = "Отчество")]
+        [StringLength(20, MinimumLength = 2)]
+        public string Patronymic { get; set; }
+
+        [Display(Name = "Серия паспорта")]
+        [MinLength(4)]
+        [MaxLength(4)]
+        public int PassportSeries { get; set; }
+
+        [Display(Name = "Номер паспорта")]
+        [MinLength(6)]
+        [MaxLength(6)]
+        public int PassportNumber { get; set; }
+
+        public DateTime DateOfBird { get; set; }
+        //Сделать невидимым
+        public string? Email { get; set; }
+    }
+}
